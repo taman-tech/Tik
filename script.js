b5c8af41cb01ecd1e2c422f81e5ee994901082bb
taman-tech <108014156+taman-tech@users.noreply.github.com>
2023-05-11 05:01:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
-$(document).ready(function() {
-  // Smooth scrolling
-  $('a[href^="#"]').on('click', function(event) {
-    var target = $(this.getAttribute('href'));
-    if (target.length) {
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
-    }
-  });
+const form = document.querySelector("form");
+const output = document.querySelector("#output");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const url = form.elements.url.value;
+  if (!url) {
+    output.innerHTML = "<p>Please enter a URL.</p>";
+    return;
+  }
+
+  const videoId = getVideoId(url);
+  if (!videoId) {
+    output.innerHTML = "<p>Invalid URL.</p>";
+    return;
+  }
+
+  const downloadUrl = getDownloadUrl(videoId);
+  if (!downloadUrl) {
+    output.innerHTML = "<p>Unable to get download URL.</p>";
+    return;
+  }
+
+  output.innerHTML = `
+    <p>Right click on the download button below and select "Save link as..." to download the video.</p>
+    <a href="${downloadUrl}" download>Download Video</a>
+  `;
 });
+
+function getVideoId(url) {
+  const match = url.match(/\/video\/(\d+)/);
+  if (match) {
+    return match[1];
+  } else {
+    return null;
+  }
+}
+
+async function getDownloadUrl(videoId) {
+  const response = await fetch(`https://www.tiktok.com/node/share/video/${videoId}`);
+  const data = await response.json();
+  const video = data.collector[0];
+  if (video) {
+    const downloadUrl = video.downloadAddr.replace(/watermark=1/, "watermark=0");
+    return downloadUrl;
+  } else {
+    return null;
+  }
+}
