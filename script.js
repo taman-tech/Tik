355c6f937d91d564aaf4cc27c2de2a0fa07ee1a1
taman-tech <108014156+taman-tech@users.noreply.github.com>
2023-05-11 04:34:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  // Smooth scrolling
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if (target.length) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+    }
+  });
+});
